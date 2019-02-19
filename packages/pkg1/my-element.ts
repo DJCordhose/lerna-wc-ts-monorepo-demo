7105e6a9fc70ffc8036ae120e484bdd1fcda2719
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-02-19 15:15:42 +0100

    complete first sample

---
+++ packages/pkg1/my-element.ts
@@ -0,0 +1,20 @@
+import 'pkg2/my-other-element'
+
+const template = `
+<p>Package 1</p>
+<other-element></other-element>
+`;
+
+class MyElement extends HTMLElement {
+    constructor() {
+        super()
+        this.render()
+    }
+
+    render() {
+        this.attachShadow({mode: 'open'})
+        this.shadowRoot!.innerHTML = template
+
+    }
+}
+customElements.define('my-element', MyElement)
